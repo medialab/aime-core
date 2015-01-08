ce27986d59dee99f03c4822c77bd58bbbe72513e
Yomguithereal <guillaumeplique@gmail.com>
2015-01-08 15:52:16 +0100

    Refining query

--- api/queries.js
+++ api/queries.js
@@ -12,13 +12,44 @@ var config = require('../config.json').dbs.neo4j,
 var host = 'http://' + config.host + ':' + config.port,
     db = new seraph(host);
 
+// Defining our own ways to invoke the api
+db.cypher = function(query, params, callback) {
+  if (typeof params === 'function') {
+    callback = params;
+    params = params || {};
+  }
+
+  var operation = db.operation(
+    'transaction/commit',
+    'POST',
+    {
+      statements: [
+        {
+          statement: query,
+          resultDataContents: ['row']
+        }
+      ],
+      parameters: params
+    }
+  );
+
+  db.call(operation, callback);
+};
+
 // List of cypher queries
 const queries = {
   book: [
-    'MATCH (b:Book {lang:{lang}})-[rc:HAS]-(c:Chapter)-[rs:HAS]-(s:Subheading)',
-    'RETURN collect(c) AS chapters;'
+    'MATCH (b:Book {lang: "en"})-[rc:HAS]-(c:Chapter)-[rs:HAS]-(s:Subheading)-[rp:HAS]-(p:Paragraph)',
+    'WITH s, c, p, rs, rc, rp, {id: id(p), properties: p} AS paragraphs ORDER BY rp.order ASC',
+    'WITH c, rs, rc, {subheading: {id: id(s), properties: s}, paragraphs: collect(paragraphs)} AS subheadings ORDER BY rs.order ASC',
+    'WITH rc, {chapter: {id: id(c), properties: c}, subheadings: collect(subheadings)} AS chapters ORDER BY rc.order ASC',
+    'RETURN chapters;'
   ].join('\n')
 };
+
+db.cypher(queries.book, function(err, results) {
+  console.log(JSON.stringify(results, null, 2));
+});
 
 // Functions
 module.exports = {
@@ -26,13 +57,3 @@ module.exports = {
     db.queryRaw(queries.book, {lang: lang}, callback);
   }
 };
-
-/*
-
-MATCH (b:Book)-[rc:HAS]-(c:Chapter)-[rs:HAS]-(s:Subheading)-[rp:HAS]-(p:Paragraph) WHERE b.lang="en"
-WITH s, c, p, rs, rc, rp ORDER BY rp.order ASC
-WITH c, rs, rc, {subheading: s, paragraphs: collect(p)} AS subheadings ORDER BY rs.order ASC
-WITH rc, {chapter: c, subheadings: collect(subheadings)} AS chapters ORDER BY rc.order ASC
-RETURN chapters;
-
-*/
