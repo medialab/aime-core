f09c95c8bacf123ba355c8ff2e686b3f6209cb58
Yomguithereal <guillaumeplique@gmail.com>
2015-01-26 17:28:36 +0100

    Improving tokenizer again and again

--- test/suites/tokenizer.js
+++ test/suites/tokenizer.js
@@ -31,8 +31,8 @@ describe('Tokenizer', function() {
 
   it('should be possible to catch abbreviations.', function() {
     assert.deepEqual(
-      tokenizer('Hello, my liege. How dost thou fare? How about your N.A.T.O. hearings? \'Twas fine!'),
-      ['Hello, my liege.', 'How dost thou fare?', 'How about your N.A.T.O. hearings?', '\'Twas fine!']
+      tokenizer('Hello, my liege. How dost thou fare? How about your N.A.T.O. hearings? It was fine!'),
+      ['Hello, my liege.', 'How dost thou fare?', 'How about your N.A.T.O. hearings?', 'It was fine!']
     );
   });
 
@@ -59,15 +59,44 @@ describe('Tokenizer', function() {
 
   it('should be possible to recompose blocks that are inside quotation marks.', function() {
     assert.deepEqual(
-      tokenizer('He said "this. is.my. horse" and nay. What can I do?'),
-      ['He said "this. is.my. horse" and nay.', 'What can I do?']
+      tokenizer('He said "this. is.my. Horse" and nay. What can I do?'),
+      ['He said "this. is.my. Horse" and nay.', 'What can I do?']
     );
   });
 
   it('should be possible to deal with a most complex case.', function() {
     assert.deepEqual(
-      tokenizer('He said "this. is.my horse." What can I do?'),
-      ['He said "this. is.my horse."', 'What can I do?']
+      tokenizer('He said "this. is not. My horse." What can I do?'),
+      ['He said "this. is not. My horse."', 'What can I do?']
+    );
+  });
+
+  it('should work with a real-life example.', function() {
+    assert.deepEqual(
+      tokenizer('So I seek no justification other than a lovely image from Souriau: the colors of the Lascaux cave are quite simply those that the painter found underfoot; “yellow ochre, red ochre; green clay, black smoke. He has to make do.” To adopt a horse.'),
+      [
+        'So I seek no justification other than a lovely image from Souriau: the colors of the Lascaux cave are quite simply those that the painter found underfoot; “yellow ochre, red ochre; green clay, black smoke. He has to make do.”',
+        'To adopt a horse.'
+      ]
+    );
+  });
+
+  it('should handle proclitics.', function() {
+    assert.deepEqual(
+      tokenizer('\'Tis a fine lad? No. \'Twas a fine lad... Ok!'),
+      ['\'Tis a fine lad?', 'No.', '\'Twas a fine lad...', 'Ok!']
+    );
+  });
+
+  it('should work when some edge cases are positioned at the end of the text.', function() {
+    assert.deepEqual(
+      tokenizer('Hello. This is Mr.'),
+      ['Hello.', 'This is Mr.']
+    );
+
+    assert.deepEqual(
+      tokenizer('Hello. I am a "falsy string.'),
+      ['Hello.', 'I am a "falsy string.']
     );
   });
 });
