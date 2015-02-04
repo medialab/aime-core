72bcc82dd897bf3f155ac76c5289d924c277aebf
Yomguithereal <guillaumeplique@gmail.com>
2015-02-04 16:04:19 +0100

    Adding doc route

---
+++ api/model/document.js
@@ -0,0 +1,48 @@
+/**
+ * AIME-core Document Model
+ * =========================
+ *
+ */
+var db = require('../connection.js'),
+    queries = require('../queries.js'),
+    _ = require('lodash');
+
+// TODO: compile queries
+// TODO: better model abstraction
+// TODO: with fracking cache
+module.exports = {
+  getAll: function(lang, callback) {
+
+    // Executing query
+    db.rows(queries.document, {lang: lang}, function(err, response) {
+
+      // On typical error
+      if (err) return callback(err);
+
+      // On REST error
+      if (response.errors.length) return callback(response.errors[0]);
+
+      // Treating incoming data
+      var data = _(response.results[0].data)
+        .map(function(line) {
+          var d = line.row[0],
+              rd = _.extend({id: d.id}, d.properties);
+
+          rd.children = line.row[1].map(function(s) {
+            var rs = _.extend({id: s.slide.id}, s.slide.properties);
+
+            rs.children = s.children.map(function(e) {
+              return _.extend({id: e.id}, e.properties);
+            });
+
+            return rs;
+          });
+
+          return rd;
+        })
+        .value();
+
+      return callback(null, data);
+    });
+  }
+};
--- api/queries.js
+++ api/queries.js
@@ -46,7 +46,18 @@ module.exports = {
   ].join('\n'),
 
   /**
+   * Retrieving documents
+   */
+  document: [
+    'MATCH (d:Document {lang: {lang}, status: "public"})-[rs:HAS]-(s:Slide)-[re:HAS]-(e)',
+    'WITH d, rs, s, re, {id: id(e), properties: e} AS elements ORDER BY re.order',
+    'WITH d, rs, s, {slide: {id: id(s), properties: s}, children: collect(elements)} AS slides ORDER BY rs.order',
+    'RETURN {id: id(d), properties: d} AS document, collect(slides) AS slides ORDER BY document.properties.title;'
+  ].join('\n'),
+
+  /**
    * Find a user with mail address and hashed password
    */
   user: 'MATCH (n:User {email: {email}, password: {hash}, active: {active}}) RETURN n;'
 };
+
