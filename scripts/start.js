8495400dcb92cc7f26556afaac6f16787a4930a0
Yomguithereal <guillaumeplique@gmail.com>
2015-02-11 14:25:14 +0100

    Adding biblib client

--- scripts/start.js
+++ scripts/start.js
@@ -4,6 +4,7 @@
  *
  * This scripts launches the express application serving the inquiry's data.
  */
-var app = require('../api/app.js');
+var app = require('../api/app.js'),
+    biblib = require('../api/model/biblib.js');
 
 app.listen(7000);
