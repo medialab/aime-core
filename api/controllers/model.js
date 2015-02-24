5c4045b23fa4e4ca198ee3c8f5797a54165f7cd5
Yomguithereal <guillaumeplique@gmail.com>
2015-02-24 11:47:54 +0100

    Refactor model controller

--- api/controllers/model.js
+++ api/controllers/model.js
@@ -10,31 +10,63 @@ var book = require('../model/book.js'),
     types = require('../typology.js'),
     _ = require('lodash');
 
+// Forge
+function getAll(model) {
+  return function(req, res) {
+    model.getAll(req.lang, function(err, result) {
+      if (err) return res.serverError(err);
+
+      return res.ok(result);
+    });
+  };
+}
+
+function search(model) {
+  return function(req, res) {
+    model.search(req.lang, req.params.query, function(err, ids) {
+      if (err) return res.serverError(err);
+
+      return res.ok(ids);
+    });
+  };
+}
+
+function getByIds(model) {
+  return function(req, res) {
+    var ids = _.uniq(req.params.ids.split(',')),
+        method;
+
+    if (types.check(ids, 'ids'))
+      method = 'getByIds';
+    else if (types.check(ids, 'slugs'))
+      method = 'getBySlugIds';
+    else
+      return res.badRequest(
+        'Wrong ids.',
+        'List of ids or slug ids separated by commas.'
+      );
+
+    model[method](ids, function(err, vocs) {
+      if (err) return res.serverError(err);
+
+      return res.ok(vocs);
+    });
+  };
+}
+
 // Refactor model access by forge
 module.exports = [
   {
     url: '/book',
     cache: 'book',
-    action: function(req, res) {
-      book.getAll(req.lang, function(err, result) {
-        if (err) return res.serverError(err);
-
-        return res.ok(result);
-      });
-    }
+    action: getAll(book)
   },
   {
     url: '/book/search/:query',
     validate: {
       query: 'string'
     },
-    action: function(req, res) {
-      book.search(req.lang, req.params.query, function(err, ids) {
-        if (err) return res.serverError(err);
-
-        return res.ok(ids);
-      });
-    }
+    action: search(book)
   },
   {
     url: '/voc',
@@ -43,49 +75,18 @@ module.exports = [
       offset: '?integer'
     },
     cache: 'vocabulary',
-    action: function(req, res) {
-      voc.getAll(req.lang, req.query, function(err, result) {
-        if (err) return res.serverError(err);
-
-        return res.ok(result);
-      });
-    }
+    action: getAll(voc)
   },
   {
     url: '/voc/:ids',
-    action: function(req, res) {
-      var ids = _.uniq(req.params.ids.split(',')),
-          method;
-
-      if (types.check(ids, 'ids'))
-        method = 'getByIds';
-      else if (types.check(ids, 'slugs'))
-        method = 'getBySlugIds';
-      else
-        return res.badRequest(
-          'Wrong ids.',
-          'List of ids or slug ids separated by commas.'
-        );
-
-      voc[method](ids, function(err, vocs) {
-        if (err) return res.serverError(err);
-
-        return res.ok(vocs);
-      });
-    }
+    action: getByIds(voc)
   },
   {
     url: '/voc/search/:query',
     validate: {
       query: 'string'
     },
-    action: function(req, res) {
-      voc.search(req.lang, req.params.query, function(err, vocs) {
-        if (err) return res.serverError(err);
-
-        return res.ok(vocs);
-      });
-    }
+    action: search(voc)
   },
   {
     url: '/doc',
@@ -94,48 +95,17 @@ module.exports = [
       offset: '?integer'
     },
     cache: 'documents',
-    action: function(req, res) {
-      doc.getAll(req.lang, req.query, function(err, result) {
-        if (err) return res.serverError(err);
-
-        return res.ok(result);
-      });
-    }
+    action: getAll(doc)
   },
   {
     url: '/doc/:ids',
-    action: function(req, res) {
-      var ids = _.uniq(req.params.ids.split(',')),
-          method;
-
-      if (types.check(ids, 'ids'))
-        method = 'getByIds';
-      else if (types.check(ids, 'slugs'))
-        method = 'getBySlugIds';
-      else
-        return res.badRequest(
-          'Wrong ids.',
-          'List of ids or slug ids separated by commas.'
-        );
-
-      doc[method](ids, function(err, vocs) {
-        if (err) return res.serverError(err);
-
-        return res.ok(vocs);
-      });
-    }
+    action: getByIds(doc)
   },
   {
     url: '/doc/search/:query',
     validate: {
       query: 'string'
     },
-    action: function(req, res) {
-      doc.search(req.lang, req.params.query, function(err, vocs) {
-        if (err) return res.serverError(err);
-
-        return res.ok(vocs);
-      });
-    }
+    action: search(doc)
   }
 ];
---
+++ api/model/abstract.js
@@ -0,0 +1,8 @@
+/**
+ * AIME-core Abstract Model
+ * =========================
+ *
+ * Collection of methods used by most of AIME-core models.
+ */
+var helpers = require('../helpers.js'),
+    _ = require('lodash');
