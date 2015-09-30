850989431798dd0875e7982acf71930446b08b97
Yomguithereal <guillaumeplique@gmail.com>
2015-09-30 11:50:03 +0200

    Starting mail send

--- api/postman.js
+++ api/postman.js
@@ -12,4 +12,17 @@ var nodemailer = require('nodemailer'),
 var transport = nodemailer.createTransport(smtp(config));
 
 // Exporting
-module.exports = transport;
+module.exports = {
+
+  // Sending the registration email
+  registration: function(lang, to, callback) {
+    var options = {
+      from: config.from,
+      to: to,
+      subject: 'AIME-Test',
+      text: 'This is some very useful text.'
+    };
+
+    transport.sendMail(options, callback);
+  }
+};
