181c0a00c794c6fa8f2811306ab2f754273cfc4a
bverjat <benoit.verjat@sciences-po.fr>
2015-10-01 12:26:46 +0200

    finalizing bookmarks on paragaphs

--- interfaces/inquiry/js/maze.domino.js
+++ interfaces/inquiry/js/maze.domino.js
@@ -1770,14 +1770,12 @@
             this.update('lang', data.result.lang);
             maze.i18n.lang = data.result.lang;
 
-            if(data.result.user) {
-
-              // for test only
-              data.result.user.bookmarks = [736,740,744];
-
-              this.update('bookmarks', data.result.user.bookmarks.reduce(
-                function(o, v, i) { o[v] = true; return o;}, {}
+            if(data.result.bookmarks) {
+              this.update('bookmarks', data.result.bookmarks.reduce(
+                  function(o, v, i) { o[v] = true; return o;}, {}
               ));
+            }
+            if(data.result.user) {
               this.update('user', data.result.user); // this is the user
             }
             this.dispatchEvent('session__initialized');
