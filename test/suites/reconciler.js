98848561c2321c826fc4ed3bcae5a5c236696f95
Yomguithereal <guillaumeplique@gmail.com>
2016-03-11 16:27:42 +0100

    Unit testing reconciler now done

--- test/suites/reconciler.js
+++ test/suites/reconciler.js
@@ -8,6 +8,10 @@ var assert = require('assert'),
 
 describe('Reconciler', function() {
   var text = {
+    padding: {
+      type: 'paragraph',
+      text: 'Padding'
+    },
     hello: {
       type: 'paragraph',
       text: 'Hello World!'
@@ -22,24 +26,61 @@ describe('Reconciler', function() {
     }
   };
 
-  var medias = {
-    quote: {
-      type: 'quote',
-      id: 1
-    },
-    image: {
-      type: 'image',
-      id: 2
-    }
+  var bookmarks = {
+    one: {index: 0, id: 1},
+    two: {index: 1, id: 2},
+    three: {index: 2, id: 3}
   };
 
   it('should properly reconcile bookmarks.', function() {
     var cases = [
+
+      // Identical
       {
-        bookmarks: [{index: 1, id: 1}],
+        bookmarks: [bookmarks.two],
         beforeItems: [text.hello, text.goodbye],
         afterItems: [text.hello, text.goodbye],
         operations: []
+      },
+
+      // Fuzzy same place
+      {
+        bookmarks: [bookmarks.two],
+        beforeItems: [text.padding, text.hello, text.goodbye],
+        afterItems: [text.padding, text.alteredHello, text.goodbye],
+        operations: [{type: 'rewire', target: text.alteredHello, bookmark: bookmarks.two}]
+      },
+
+      // Fuzzy different place
+      {
+        bookmarks: [bookmarks.one],
+        beforeItems: [text.hello, text.goodbye],
+        afterItems: [text.goodbye, text.alteredHello],
+        operations: [{type: 'rewire', target: text.alteredHello, bookmark: bookmarks.one}]
+      },
+
+      // Force same place
+      {
+        bookmarks: [bookmarks.two],
+        beforeItems: [text.hello, text.goodbye],
+        afterItems: [text.hello, text.padding],
+        operations: [{type: 'rewire', target: text.padding, bookmark: bookmarks.two}]
+      },
+
+      // Force first item
+      {
+        bookmarks: [bookmarks.two],
+        beforeItems: [text.hello, text.goodbye],
+        afterItems: [text.hello],
+        operations: [{type: 'rewire', target: text.hello, bookmark: bookmarks.two}]
+      },
+
+      // Wiped
+      {
+        bookmarks: [bookmarks.three],
+        beforeItems: [text.padding, text.hello, text.goodbye],
+        afterItems: [],
+        operations: [{type: 'drop', bookmark: bookmarks.three}]
       }
     ];
 
