5a52fba2bb73fdd6a5d6f74ac9270abaa9bb1ec9
Yomguithereal <guillaumeplique@gmail.com>
2016-03-11 15:02:06 +0100

    Unit testing jaccard

--- test/endpoint.js
+++ test/endpoint.js
@@ -5,6 +5,7 @@
  * File in charge of requiring the test suites.
  */
 require('./suites/diff.js');
+require('./suites/jaccard.js');
 require('./suites/markdown_stripper.js');
 require('./suites/modecross_parser.js');
 require('./suites/tokenizer.js');
