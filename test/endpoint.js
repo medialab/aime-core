6a959e1d32feea4889a418002cb81e7013d7dda1
Yomguithereal <guillaumeplique@gmail.com>
2016-05-26 10:33:39 -0700

    Replacing Jaccard with a normalized Levenshtein

--- test/endpoint.js
+++ test/endpoint.js
@@ -5,7 +5,6 @@
  * File in charge of requiring the test suites.
  */
 require('./suites/diff.js');
-require('./suites/jaccard.js');
 require('./suites/markdown_stripper.js');
 require('./suites/modecross_parser.js');
 require('./suites/reconciler.js');
